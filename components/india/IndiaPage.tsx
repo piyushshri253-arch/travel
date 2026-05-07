fa134b8773af1ddcfd580621f8b04d81bf4ac5f3
Piyush Srivastava <piyushshri253@gmail.com>
2026-05-07 23:31:54 +0530

    fix: remove large file + add gitignore

---
+++ components/india/IndiaPage.tsx
@@ -0,0 +1,25 @@
+import Hero from "../international/hero";
+import CommunityTrips from "../international/CommunityTrips";
+import Exclusive from "../international/Exclusive";
+import Uk from "../international/uk";
+import FAQ from "../international/Faq";
+
+import { indiaData } from "@/data/indiaData";
+
+type Props = {
+  country: keyof typeof indiaData;
+};
+
+export default function IndiaPage({ country }: Props) {
+  const data = indiaData[country];
+
+  return (
+    <>
+      <Hero data={data} />
+      {/* <CommunityTrips />
+      <Exclusive />
+      <Uk /> */}
+      <FAQ />
+    </>
+  );
+}
